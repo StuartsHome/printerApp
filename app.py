aa17d114b3341359db3db01bd15a262a4a1d2d72
Stuart Smith <stuartsayswhat@googlemail.com>
2021-01-23 11:44:09 +0000

    todo

---
+++ app.py
@@ -0,0 +1,14 @@
+
+# To do:
+
+# 1. Create Flask App
+# 2. Run in Docker Container
+# 3. Connect Database. Which one? (mySQL, PostGres)
+    # Possibly PostGres
+# 4. FrontEnd in REACT
+
+
+
+
+if __name__ == '__main__':
+    pass
