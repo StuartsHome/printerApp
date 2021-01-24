78a533e75745b3c4749d9cadaacc1b845ec7edc7
Stuart Smith <stuartsayswhat@googlemail.com>
2021-01-24 18:26:57 +0000

    added .env file

--- src/server.py
+++ src/server.py
@@ -1,4 +1,19 @@
+
 from flask import Flask
+import os
+from dotenv import load_dotenv
+"""
+Dotenv lifecycle:
+1. import module: from dotenv import ...
+2. load_dotenv()
+3. The key/value pairs from the .env file is now present as system environmental variables
+    and can be accessed via:
+4. os.getenv()
+"""
+
+load_dotenv()
+SECRET_KEY = os.getenv("EMAIL")
+
 server = Flask(__name__)
 
 
@@ -8,4 +23,5 @@ def hello():
 
 
 if __name__ == '__main__':
+    print(SECRET_KEY)
     server.run(host='0.0.0.0')
