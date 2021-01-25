d29c573d13cb07823ce2bc8760870d5a89096685
Stuart Smith <stuartsayswhat@googlemail.com>
2021-01-25 18:10:31 +0000

    more code

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
-
-from flask import Flask
+import requests
+from flask import Flask, render_template, request
 import os
 from dotenv import load_dotenv
 """
@@ -14,14 +14,31 @@ Dotenv lifecycle:
 load_dotenv()
 SECRET_KEY = os.getenv("EMAIL")
 
-server = Flask(__name__)
+app = Flask(__name__)
 
+"""
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+"""
 
-@server.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
     print(SECRET_KEY)
-    server.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
